a9c1b0f1e9b9a50d0374e91a54c96f13d0594bae
Aman Damani <amandamani007@gmail.com>
2023-10-29 15:45:37 -0400

    Server working with get requests

--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+
+const apiData = require('../data/videos.json');
 
 router
 .get('/', (req,res) => {
-    res.send("Get videos list");
+    res.status(200);
+    res.json(apiData.videos);
 })
 .get('/:id', (req,res) => {
     const videoId = req.params.id;
-    res.send(`Get video ${videoId}`);
+    res.status(200)
+    res.json(apiData.videDetails.filter((element) => element.id === videoId)[0]);
 });
 
 module.exports = router;
