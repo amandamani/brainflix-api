12535024b1f1d275125eb5212dc17cc9abc20618
Aman Damani <amandamani007@gmail.com>
2023-10-29 23:09:20 -0400

    Server working with get and post requests.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const {PORT, BACKEND_URL, CORS_ORIGIN} = process.env;
 const express = require('express');
 const app = express();
+app.use(express.static('public'))
 const cors = require('cors');
 const videoRoutes = require('./routes/videos');
 
--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-
+router.use(express.json());
 const apiData = require('../data/videos.json');
 
 router
@@ -13,6 +13,33 @@ router
     const videoId = req.params.id;
     res.status(200)
     res.json(apiData.videDetails.filter((element) => element.id === videoId)[0]);
-});
+})
+.post('/upload', (req,res) => {
+    res.status(200).send("Post complete");
+    const {id, title, description} = req.body;
+    let video = {};
+    let videoDetail = {}
+    video.id = id;
+    video.title = title;
+    video.channel = "Aman Damani";
+    video.image = "http://localhost:8080/images/Upload-video-preview.jpg";
+    videoDetail = {...video};
+    videoDetail.description = description;
+    videoDetail.views = "786110786";
+    videoDetail.likes = "786110786";
+    videoDetail.duration = "5:00";
+    videoDetail.timestamp = Date.now();
+    videoDetail.comments = [];
+    apiData.videos.push(video);
+    apiData.videDetails.push(videoDetail);
+    fs.writeFile('data/videos.json', JSON.stringify(apiData),(err) => {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          console.log('Videos file successfully updated.');
+        }
+      });
+})
 
 module.exports = router;
